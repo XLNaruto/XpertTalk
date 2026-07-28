05260bd067df8ea928372b4c1be038a2df8ea5d4
XL Naruto <xpertlab.naruto@gmail.com>
2026-07-28 09:19:50 +0530

    changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-b6866b34'], (function (workbox) { 'use strict';
     "revision": "a6f83a9e953bede3b148a975e3d9a0d5"
   }, {
     "url": "index.html",
-    "revision": "0.7k9k3b4075c"
+    "revision": "0.f1683lgsna8"
   }], {
     "ignoreURLParametersMatching": [/^utm_/, /^fbclid$/]
   });
